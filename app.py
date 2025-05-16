63252e8bf7792cebfdaa75fceab609bd032146ea
Duc.Nguyentien <saccarozo04@gmail.com>
2025-05-16 19:06:10 +0700

    Fix

--- app.py
+++ app.py
@@ -90,4 +90,6 @@ def quiz(quiz_id):
                            user_answers=user_answers)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
